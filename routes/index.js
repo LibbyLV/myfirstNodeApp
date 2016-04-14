4bfd88e18325bedeb0cec854df86677edae7fbf6
LRF <shadowkiller028@gmail.com>
2016-04-14 17:12:08 +0800

    login/out check

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,55 @@ var flash = require('connect-flash');
 app.get('/', function (req, res) {
 
 
-               res.render('index', { title: '主页' });
+     res.render('index', { title: '主页',
+     user: req.session.user,
+     success: req.flash('success').toString(),
+     error: req.flash('error').toString()});
 
 
 });
 
 app.get('/login', function (req, res) {
-               res.render('login', {title: '登录'});
-});
-
-
-app.post('/login', function (req, res) {
-               // res.send('login',{title:'登录'});
+               res.render('index', { title: '登录',
+               user: req.session.user,
+               success: req.flash('success').toString(),
+               error: req.flash('error').toString()});
 });
 
 app.get('/reg', function (req, res) {
-               res.render('reg', { title: '注册' });
+               res.render('reg', { title: '注册',
+                              user: req.session.user,
+                              success: req.flash('success').toString(),
+                              error: req.flash('error').toString() });
 });
+app.post('/login', function (req, res) {
+//生成密码的 md5 值
+      var md5 = crypto.createHash('md5'),
+      password = md5.update(req.body.password).digest('hex');
+      //check user existed?
+      User.get(req.body.name,function(err,user){
+           if(err)
+           {
+             req.flash('error',err);
+             return res.redirect('/');
+           }
+           if(!user){
+             req.flash('err','user is not existed');
+             return res.redirect('/login');
+           }
+           if(password!=user.password){
+             req.flash('err','passwd is wrong');
+             return res.redirect('/login');
+           }
+           req.session.user = user;
+           req.flash('success','login OK');
+           return res.redirect('/');
+
+      });
+
+});
+
+
 
 app.post('/reg', function (req, res) {
                console.log("1");
@@ -90,8 +122,21 @@ app.post('/post', function (req, res) {
 });
 
 app.get('/logout', function (req, res) {
-
+ req.session.user = null;
+ req.flash('success','logout OK');
+ return res.redirect('/');
 });
 
-
+function checkNotlogin(req,res){
+   if(req.body.user){
+     req.flash('error','already login');
+     return res.redirect('back');
+   }
+}
+function checklogin(req,res){
+   if(!req.body.user){
+      req.flash('error','Not login');
+      return res.redirect('/login');
+   }
+}
 module.exports = app;
